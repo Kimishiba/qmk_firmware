33d03c2e10232059b6976a17d054e69707b06bef
Kimishiba <75455353+Kimishiba@users.noreply.github.com>
2026-05-21 13:54:45 +0200

    Add files via upload

---
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -0,0 +1,5 @@
+# crkbd default keymap — rules.mk
+# Place this file at:
+#   keyboards/crkbd/keymaps/default/rules.mk
+
+MOUSEKEY_ENABLE = yes   # Required for KC_MS_UP / KC_MS_DOWN / KC_MS_LEFT / KC_MS_RIGHT
